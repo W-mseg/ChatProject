c4f21677a683a2857e5150709ff35218558b6ffd
W-mseg <marco.segretario2705@gmail.com>
2020-03-17 14:49:33 +0100

    update

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <div id="Landing">
     <a href="./loginpage.php" class="button" id="LoginButton">Login</a>
     <br>
-    <a href="#" class="button" id="RegisterButton">Register</a>
+    <a href="./registerpage.php" class="button" id="RegisterButton">Register</a>
     <br>
     <a href="#" class="button" id="ChatButton">Chat</a>
 </div>
---
+++ registerpage.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="fr">
+<head>
+<?php include('./component/head.php') ?>
+</head>
+<body>
+<div id="registerform">
+    <input type="text" placeholder="pseudo" name="registername">
+    <br>
+    <input type="text" placeholder="password" name="registerpassword">
+</div>
+</body>
+</html>
